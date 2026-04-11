e11be01f729159658ba54b4c5809ee48ed7e1b51
Suyash Kumar Tiwari <suyashkumar6337@gmail.com>
2026-04-11 12:19:22 +0530

    Time: 0 ms (100%), Space: 8.9 MB (30.68%) - LeetHub

---
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+        int n=s.length();
+
+        for(int i=0;i<=n/2;i++){
+            if(s[i]==s[n-i-1]) return i;
+        }
+        return -1;
+    }
+};
